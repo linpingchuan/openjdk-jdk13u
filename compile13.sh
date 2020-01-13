824372247be9a73653eb979f00f6247ac2f476b8
linpingchuan <linpingchuan@uzoo.cn>
2020-01-13 17:39:41 +0800

    修改编译脚本文件

--- compile13.sh
+++ compile13.sh
@@ -1,25 +1,37 @@
 #!/bin/bash
 
 compile_openjdk13u(){
+    is_git_f=$1
+    cd /home/lin/openjdk-jdk13u/
+    
+    if [ "$is_git_f" = "f" ];then
+	    git pull -f
+    fi
+    
     chuan apt-get install -y libx11-dev libxext-dev libxrender-dev libxrandr-dev libxtst-dev libxt-dev
     chuan apt-get install ccache -y
-    cd /home/lin/projects/openjdk-jdk13u
-    # chuan make dist-clean
-    current_directory=`pwd | grep -a 'jdk' | grep -a 'u'`
-    if [ -n "$current_directory" ];then
-        # 指定 bootjdk 为 openjdk13
-        bash configure --with-debug-level=slowdebug --disable-warnings-as-errors \
-                --with-target-bits=64 \
-                --enable-ccache --with-native-debug-symbols=external \
-                --with-boot-jdk=/opt/jdk-13.0.1/
-    else
-        bash configure --with-debug-level=slowdebug --disable-warnings-as-errors --enable-ccache --with-native-debug-symbols=external
+    
+    cd /home/lin/openjdk-jdk13u/build/linux-x86_64-server-slowdebug/jdk/bin
+    has_directory=$?
+    if [ "$has_directory" = "0" ];then
+	    cd /home/lin/openjdk-jdk13u/
+	    # chuan make dist-clean
+	    current_directory=`pwd | grep -a 'jdk' | grep -a 'u'`
+	    if [ -n "$current_directory" ];then
+	        # 指定 bootjdk 为 openjdk13
+	       bash configure --with-debug-level=slowdebug --disable-warnings-as-errors \
+        	       --with-target-bits=64 \
+        	       --enable-ccache --with-native-debug-symbols=external \
+	               --with-boot-jdk=/opt/jdk-13.0.1/
+	       chuan make hotspot-only CONF=linux-x86_64-server-slowdebug JOBS=1
+
+	       export PATH=$PATH:/home/lin/openjdk-jdk13u/build/linux-x86_64-server-slowdebug/jdk/bin/
+	       echo "已经完成编译啦，快去调试吧! $date"
+	    else
+		    echo "无此目录，无法编译"
+	    fi
     fi
-    # chuan make clean CONF=linux-x86_64-normal-server-fastdebug
-    # chuan make clean CONF=linux-x86_64-normal-server-slowdebug
-
-    chuan make CONF=linux-x86_64-server-slowdebug 
-
-    export PATH=$PATH:/home/lin/openjdk-jdk13u/build/linux-x86_64-server-slowdebug/jdk/bin/
 }
-compile_openjdk13u
+
+compile_openjdk13u $*
+
